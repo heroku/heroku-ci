3348e0141f27cd59145850a239f6da9811b3ac50
max beizer <max.beizer@gmail.com>
2016-11-08 13:19:07 -0600

    Try again with more full stub of last run

--- test/commands/ci/last-test.js
+++ test/commands/ci/last-test.js
@@ -22,14 +22,31 @@ describe('heroku ci:last', function () {
 
   describe('when pipeline has runs', function () {
     it('displays the results of the latest run', function () {
+      let lastRun = {
+        app_setup: { id: '4932044c-d469-4ca3-8350-0a23b8af51ea' },
+        created_at: '2016-11-08T19:02:15+00:00',
+        commit_branch: 'master',
+        commit_message: 'Add buildpacks to app.json',
+        commit_sha: 'master',
+        actor_email: 'foo@heroku.com',
+        error_status: 'running',
+        exit_code: 1,
+        id: '5835ac0f-a082-4e48-85b1-77c77797d03b',
+        message: 'Error executing \'test\' script',
+        number: 304,
+        organization: null,
+        status: 'failed',
+        updated_at: '2016-11-08T19:02:38+00:00',
+        user: { id: 'some-heroku-user-id' }
+      }
       let api = nock('https://api.heroku.com')
         .get(`/apps/${app}/pipeline-couplings`)
         .reply(200, coupling)
         .get(`/pipelines/${coupling.pipeline.id}/test-runs`)
-        .reply(200, [{ number: 251 }])
+        .reply(200, [lastRun])
 
       return cmd.run({ app }).then(() => {
-        expect(cli.stdout).to.contain(`=== Test run #251 setup`)
+        expect(cli.stdout).to.contain(`=== Test run #${lastRun.number} setup`)
         api.done()
       })
     })
