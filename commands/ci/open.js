80b8a58926945a222086c0c88865932abab7785d
max beizer <max.beizer@gmail.com>
2017-03-20 08:41:40 -0500

    Add heroku ci:open to show CI on Dashbaord from CLI

---
+++ commands/ci/open.js
@@ -0,0 +1,23 @@
+const cli = require('heroku-cli-util')
+const co = require('co')
+const opn = require('opn')
+const api = require('../../lib/heroku-api')
+
+function* run (context, heroku) {
+  const coupling = yield api.pipelineCoupling(heroku, context.app)
+  const couplingPipelineID = coupling.pipeline.id
+
+  opn(`https://dashboard.heroku.com/pipelines/${couplingPipelineID}/tests`)
+
+  return true
+}
+
+module.exports = {
+  topic: 'ci',
+  command: 'open',
+  needsApp: true,
+  needsAuth: true,
+  description: 'open the Dashboard version of Heroku CI',
+  help: 'opens a browser to view the Dashboard version of Heroku CI',
+  run: cli.command(co.wrap(run))
+}
